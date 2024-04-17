d9230dbf5bd014e80d6c38e231ac87a0b1846427
188217-LTDU\THIEN DU <lythiendu2507@gmail.com>
2024-04-17 09:01:52 +0700

    init

---
+++ src/Page/Home.js
@@ -0,0 +1,24 @@
+import React from "react";
+import SearchBar from "../Components/SearchBar/SearchBar";
+import ProductTable from '../Components/ProductTable/ProductTable';
+import { useState } from "react";
+function FilterableProductTable({ products }) {
+  const [filterText, setFilterText] = useState('');
+  const [inStockOnly, setInStockOnly] = useState(false);
+
+  return (
+    <div>
+      <SearchBar 
+        filterText={filterText} 
+        inStockOnly={inStockOnly} 
+        onFilterTextChange={setFilterText} 
+        onInStockOnlyChange={setInStockOnly} />
+      <ProductTable 
+        products={products} 
+        filterText={filterText}
+        inStockOnly={inStockOnly} />
+    </div>
+  );
+}
+
+  export default FilterableProductTable
